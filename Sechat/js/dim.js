b6fb7bea03ac106549e4b66039465e7a6d55107d
Moky <albert.moky@gmail.com>
2020-03-17 10:26:18 +0800

    Group storage OK

--- Sechat/js/dim.js
+++ Sechat/js/dim.js
@@ -1210,6 +1210,55 @@
     ns.db.UserTable = UserTable
 }(DIMP);
 ! function(ns) {
+    var Storage = ns.stargate.SessionStorage;
+    var Facebook = ns.Facebook;
+    var load = function(name) {
+        return Storage.loadJSON(name)
+    };
+    var save = function(name, value) {
+        return Storage.saveJSON(value, name)
+    };
+    var GroupTable = function() {
+        this.memberTable = {}
+    };
+    GroupTable.prototype.saveMembers = function(members, group) {
+        if (!members) {
+            return false
+        }
+        this.memberTable[group] = members;
+        return save(group, members)
+    };
+    GroupTable.prototype.loadMembers = function(group) {
+        var members = this.memberTable[group];
+        if (members) {
+            return members
+        }
+        members = [];
+        var list = load(group);
+        if (list) {
+            var facebook = Facebook.getInstance();
+            var item;
+            for (var i = 0; i < list.length; ++i) {
+                item = facebook.getIdentifier(list[i]);
+                if (!item) {
+                    throw Error("ID error: " + list[i])
+                }
+                members.push(item)
+            }
+        }
+        this.memberTable[group] = members;
+        return members
+    };
+    var s_instance = null;
+    GroupTable.getInstance = function() {
+        if (!s_instance) {
+            s_instance = new GroupTable()
+        }
+        return s_instance
+    };
+    ns.db.GroupTable = GroupTable
+}(DIMP);
+! function(ns) {
     var KeyStore = ns.KeyStore;
     var s_key_store = null;
     KeyStore.getInstance = function() {
