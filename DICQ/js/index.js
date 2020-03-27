ce9e814695c2916dde0a0a1ef06fc2be5dc7f5f9
Moky <albert.moky@gmail.com>
2020-03-27 17:34:57 +0800

    deploy test environment

--- DICQ/js/index.js
+++ DICQ/js/index.js
@@ -194,13 +194,15 @@ if (typeof dicq !== 'object') {
 
     var tarsier_ui = [
         // 'https://moky.github.io/Tarsier/build/tarsier-ui.js',
-        'http://terminal.dim.chat/Tarsier/tarsier-ui.js',
+        // 'http://terminal.dim.chat/Tarsier/tarsier-ui.js',
+        'http://134.175.87.98/demo-web/Tarsier//tarsier-ui.js',
         null
     ];
     if (release) {
         tarsier_ui = [
             // 'https://moky.github.io/Tarsier/build/tarsier-ui.min.js',
-            'http://terminal.dim.chat/Tarsier/tarsier-ui.min.js',
+            // 'http://terminal.dim.chat/Tarsier/tarsier-ui.min.js',
+            'http://134.175.87.98/demo-web/Tarsier//tarsier-ui.min.js',
             null
         ];
     }
